e8fa6ea9264a0d8a2f6e2f0571f8ffc0f37afe5b
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-08-27 10:05:11 -0300

    Update ftp_version.py

--- ftp_version.py
+++ ftp_version.py
@@ -1,7 +1,7 @@
 import requests
 # find and save the current Github release
 html = str(
-    requests.get('https://github.com/minio/minio/releases/latest')
+    requests.get('https://github.com/minio/mc/releases/latest')
     .content)
 index = html.find('commits')
 github_version = html[index + 16:index + 36]
@@ -12,17 +12,17 @@ file.close()
 # find and save the current Bazel version on FTP server
 html = str(
     requests.get(
-        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/minio/'
+        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/minio-mc/'
     ).content)
-index = html.rfind('minio-')
-ftp_version = html[index + 6:index + 26]
+index = html.rfind('mc-')
+ftp_version = html[index + 3:index + 23]
 file = open('ftp_version.txt', 'w')
 file.writelines(ftp_version)
 file.close()
 
 # find and save the oldest Bazel version on FTP server
-index = html.find('minio-')
-delete = html[index + 6:index + 26]
+index = html.find('mc-')
+delete = html[index + 3:index + 23]
 file = open('delete_version.txt', 'w')
 file.writelines(delete)
 file.close()
