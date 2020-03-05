ba73c9b5b994e18ea51ca3da75c5086949dcadd3
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-03-05 00:05:54 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #github_version=$(cat github_version.txt)
 #ftp_version=$(cat ftp_version.txt)
-github_version=2020-01-16T22-40-29Z
+github_version=2020-01-25T02-50-51Z
 #ftp_version=2019-12-30T05-45-39Z
 del_version=$(cat delete_version.txt)
 
