4b34416c9b1042eae95cbd6dac05eeb0125bc255
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-06-11 21:35:51 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ then
     cd minio
     #sudo sh -c 'echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
     make
-    ./minio
+    ./minio --version
     mv minio minio-$github_version
     
     if [[ "$github_version" > "$ftp_version" ]]
