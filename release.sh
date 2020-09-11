a3a49b5d9999c3f794e6a498188e1174c6af9d3b
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2020-09-11 10:30:31 -0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -20,6 +20,6 @@ fi
 
 if [[ $github_version > $ftp_version ]]
    then
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minio/out/minio-$github_version-ppc64le.deb"
+        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minio/out/minio-$github_version-ppc64le.deb"
         sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO2 $ROOTPATH/minio-$github_version-1.ppc64le.rpm"
 fi
