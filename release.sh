ce89123b69d4da2659b9a138cfb0243bf2374ff5
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2020-09-11 11:22:24 -0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
-LOCALPATH=/minio/output
+ROOTPATH="~/rpmbuild/RPMS/ppc64le"
+LOCALPATH=/home/travis/gopath/minio/minio-$github_version
 REPO1="/repository/debian/ppc64el/minio"
 REPO2="/repository/rpm/ppc64le/minio"
 
-if [ $github_version != $ftp_version ] && [ ${dist_version} == "2020-09-10T22-02-45Z" ]
+if [ "$github_version" != "$ftp_version" ]
   then
     git clone https://$USERNAME:$TOKEN@github.com/Unicamp-OpenPower/repository-scrips.git
     cd repository-scrips/
@@ -14,12 +15,12 @@ if [ $github_version != $ftp_version ] && [ ${dist_version} == "2020-09-10T22-02
     sudo mv empacotar-deb.sh ..$LOCALPATH
     sudo mv empacotar-rpm.sh ..$LOCALPATH
     cd ..$LOCALPATH
-    sudo ./empacotar-deb.sh minio minio_ppc64le_$github_version $github_version " "
-    sudo ./empacotar-rpm.sh minio minio_ppc64le_$github_version $github_version " " "MinIO is a High Performance Object Storage released under Apache License v2.0. It is API compatible with Amazon S3 cloud storage service. Use MinIO to build high performance infrastructure for machine learning, analytics and application data workloads."
+    sudo ./empacotar-deb.sh minio minio-$github_version $github_version " "
+    sudo ./empacotar-rpm.sh minio minio-$github_version $github_version " " "MinIO is a High Performance Object Storage released under Apache License v2.0. It is API compatible with Amazon S3 cloud storage service. Use MinIO to build high performance infrastructure for machine learning, analytics and application data workloads."
 fi
 
 if [[ $github_version > $ftp_version ]]
    then
-        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minio/out/minio-$github_version-ppc64le.deb"
+        sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/minio-$github_version-ppc64le.deb"
         sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO2 $ROOTPATH/minio-$github_version-1.ppc64le.rpm"
 fi
